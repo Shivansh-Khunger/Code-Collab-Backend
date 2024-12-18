d43f5342415c4c465e2af6dbdb08a799ce83205c
shaktiDD <shaktidhardubey7@gmail.com>
2024-12-18 14:02:33 +0530

    final auth

--- api/user_routes.js
+++ api/user_routes.js
@@ -27,24 +27,43 @@ userRouter.post("/signup", async (req, res) => {
   });
 });
 
-userRouter.get("/signin", async (req, res) => {
-  const { email, password } = req.body;
-  const existingUser = await userModel.findOne({email:email});
-  if(!existingUser){
-    res.json({
-      message: "User does not exist",
-    });
-  }
-  const validPassword = await bcrypt.compare(password, existingUser.password);
-  if(!validPassword){
-    res.json({
-      message: "Invalid Password",
-    });
-  }
-  res.json({
-    message: "Signin",
-  });
+userRouter.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
 
+    // Find user by email
+    const existingUser = await userModel.findOne({ email: email });
+
+    // Check if user exists
+    if (!existingUser) {
+      return res.status(404).json({
+        message: "User does not exist",
+      });
+    }
+
+    // Compare passwords
+    const validPassword = await bcrypt.compare(password, existingUser.password);
+
+    // Check if password is correct
+    if (!validPassword) {
+      return res.status(401).json({
+        message: "Invalid Password",
+      });
+    }
+
+    // Successful signin
+    res.status(200).json({
+      message: "Signin successful",
+    });
+
+  } catch (error) {
+    
+    console.error("Signin error:", error);
+    res.status(500).json({
+      message: "Internal server error",
+      error: error.message
+    });
+  }
 });
 
 userRouter.get("/validateToken", async (req, res) => {});
